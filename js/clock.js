2847731346efac4781d134670a05c862c940501a
Donggyu Lee <dongq0513@gmail.com>
2023-03-12 20:41:30 +0900

    d-day 추가

--- js/clock.js
+++ js/clock.js
@@ -1,25 +1,28 @@
-const clock = document.querySelector("#clock");
+let clock = document.querySelector("#clock");
+
 const boxMonth = document.getElementById('box_month');
 const boxDate = document.getElementById('box_date');
 const boxYear = document.getElementById('box_year');
 const boxDay = document.getElementById('box_day');
 
 let nowDate = new Date();
+
 let year = nowDate.getFullYear();
 let month = String(nowDate.getMonth() + 1).padStart(2, '0');
 let weekDay = ['일', '월', '화', '수', '목', '금', '토']
 let day = weekDay[nowDate.getDay()];
-let date = String(nowDate.getDate()).padStart(2, '0');
-boxMonth.innerText = ` ${month}월`;
-boxDate.innerText = `${date}일`;
+let dateNow = String(nowDate.getDate()).padStart(2, '0');
+
+boxMonth.innerText = `${month}월`;
+boxDate.innerText = `${dateNow}일`;
 boxYear.innerText = `${year}년`;
 boxDay.innerText = `${day}요일`;
 
-function getClock() {
+function timeSet() {
   let hours = String(nowDate.getHours()).padStart(2, "0");
   let mins = String(nowDate.getMinutes()).padStart(2, "0");
   let sec = String(nowDate.getSeconds()).padStart(2, "0");
   clock.innerText = `${hours}:${mins}:${sec}`;
 } 
-getClock();
-setInterval(getClock, 1000);
+timeSet();
+setInterval(timeSet, 1000);
--- js/weather.js
+++ js/weather.js
@@ -1,3 +1,26 @@
+const API_KEY = "76acc525f0823946782163644c560d3c";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+  fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    const nowLocation = document.getElementById('now_location');
+    const nowLocationWeather = document.getElementById('now_location_weather');
+    const nowLocationTemperature = document.getElementById('now_location_temperature');
+    nowLocation.innerText = '현재 위치 : '+data.name;
+    nowLocationWeather.innerText = '현재 날씨 : ' +data.weather[0].main;
+    nowLocationTemperature.innerText = '현재 기온 : ' + data.main.temp +' 도';
+  });
+}
+function onGeoErr() {
+  alert("can't find you.");
+}
+
+navigator.geolocation.getCurrentPosition( onGeoOk, onGeoErr)
+
 const weather = document.getElementById('weather');
 const temperature = document.getElementById('temperature');
 
@@ -18,3 +41,4 @@ function getWeather() {
     locationWeather.appendChild(li);
   })
 }
+
